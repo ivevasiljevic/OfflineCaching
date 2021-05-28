d0d53b665017d9be992759da1cb5190c77b2312f
Ive Vasiljevic <ivasil@Ives-MBP.localdomain>
2021-05-28 14:31:39 +0200

    add data classes, add rec view adapter

---
+++ app/src/main/java/com/personal/OfflineCaching/data/Restaurant.kt
@@ -0,0 +1,9 @@
+package com.personal.OfflineCaching.data
+
+data class Restaurant(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val logo: String,
+    val address: String
+)
---
+++ app/src/main/java/com/personal/OfflineCaching/features/restaurants/RestaurantActivity.kt
@@ -0,0 +1,12 @@
+package com.personal.OfflineCaching.features.restaurants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.personal.OfflineCaching.R
+
+class RestaurantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant)
+    }
+}
